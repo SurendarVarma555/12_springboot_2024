b2df1cc37bf717e0901af19f612e03514d6f41ff
Surendar <batthulasurendar@gmail.com>
2024-07-16 11:29:55 +0530

    initial commit

---
+++ user-task-app/src/test/java/com/usertask/app/UserTaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usertask.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
