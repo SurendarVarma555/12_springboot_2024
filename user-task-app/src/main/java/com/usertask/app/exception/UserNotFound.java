5802a872bc99a2b7e15432e75ba594c9b2d2773f
Surendar <batthulasurendar@gmail.com>
2024-07-17 16:10:29 +0530

    handled exception

---
+++ user-task-app/src/main/java/com/usertask/app/exception/UserNotFound.java
@@ -0,0 +1,17 @@
+package com.usertask.app.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFound extends RuntimeException {
+
+    private String message;
+
+    public UserNotFound (String message){
+        super(message);
+        this.message = message;
+
+    }
+
+}
