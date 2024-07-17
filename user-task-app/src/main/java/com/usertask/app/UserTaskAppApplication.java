542ccac2a03d886fad8d3de393722fbf79e4d39f
Surendar <batthulasurendar@gmail.com>
2024-07-17 10:56:53 +0530

    added modelmapper

--- user-task-app/src/main/java/com/usertask/app/UserTaskAppApplication.java
+++ user-task-app/src/main/java/com/usertask/app/UserTaskAppApplication.java
@@ -1,13 +1,20 @@
 package com.usertask.app;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserTaskAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserTaskAppApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
